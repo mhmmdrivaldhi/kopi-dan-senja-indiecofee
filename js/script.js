d4d1f8164fd343fda3b88a2cb4787c58158735cf
Muhammad Rivaldhi <rivaldhyipal04@gmail.com>
2024-11-17 22:12:43 +0700

    Landing Page Website

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle Class Active
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+};
+
+// hide sidebar outside navbar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+}) 
